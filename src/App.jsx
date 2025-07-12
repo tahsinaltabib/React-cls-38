3d11eb02272af9cfed901c1229528e470794a7d2
tahsinaltabib <nabilarah33@gmail.com>
2025-07-12 20:33:36 +0600

    2nd day practice

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>Im Tahsin Al Tabib</div>
+    <div>Im Tahsin Al Tabib
+      <div>I'm from Bangladesh.</div>
+    </div>
   )
 }
 
